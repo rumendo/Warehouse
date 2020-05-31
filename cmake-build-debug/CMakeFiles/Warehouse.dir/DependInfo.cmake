7ada9200fe878646ef108423ae6e6aaceace7af2
rumendo <rumen_do@abv.bg>
2020-05-31 21:12:21 +0300

    Added CMake files

---
+++ cmake-build-debug/CMakeFiles/Warehouse.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rumen/CLionProjects/Warehouse/Date.cpp" "/home/rumen/CLionProjects/Warehouse/cmake-build-debug/CMakeFiles/Warehouse.dir/Date.cpp.o"
+  "/home/rumen/CLionProjects/Warehouse/Product.cpp" "/home/rumen/CLionProjects/Warehouse/cmake-build-debug/CMakeFiles/Warehouse.dir/Product.cpp.o"
+  "/home/rumen/CLionProjects/Warehouse/Storage.cpp" "/home/rumen/CLionProjects/Warehouse/cmake-build-debug/CMakeFiles/Warehouse.dir/Storage.cpp.o"
+  "/home/rumen/CLionProjects/Warehouse/Warehouse.cpp" "/home/rumen/CLionProjects/Warehouse/cmake-build-debug/CMakeFiles/Warehouse.dir/Warehouse.cpp.o"
+  "/home/rumen/CLionProjects/Warehouse/main.cpp" "/home/rumen/CLionProjects/Warehouse/cmake-build-debug/CMakeFiles/Warehouse.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Warehouse.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Warehouse.dir/main.cpp.o"
+  "CMakeFiles/Warehouse.dir/Product.cpp.o"
+  "CMakeFiles/Warehouse.dir/Date.cpp.o"
+  "CMakeFiles/Warehouse.dir/Warehouse.cpp.o"
+  "CMakeFiles/Warehouse.dir/Storage.cpp.o"
+  "Warehouse.pdb"
+  "Warehouse"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Warehouse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
